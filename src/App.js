f3de06739b90b5af091fa2bca65f62838d30c8e6
Navnath0006 <43891156+Navnath0006@users.noreply.github.com>
2023-02-25 17:07:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-var headingText = "Like me";
+var headingText = "ॐ नमः शिवाय ";
 var bgcolor = "red";
 var color = "yellow";
 var color1 = "#be123c";
@@ -13,18 +13,25 @@ export default function App() {
     // count = count + 1;
     // console.log("clicked!", count);
     var newLikeCounterValue = likeCounter + 1;
+    if (newLikeCounterValue === 108) {
+      console.log("hari om");
+    }
     setLikeCounter(newLikeCounterValue);
   }
   // console.log("like counter value :", likeCounter);
   return (
     <div className="App">
       <h1 style={{ backgroundColor: bgcolor, color: color }}> {headingText}</h1>
+      <h4>#click on ॐ नमः शिवाय button for counting your chanting</h4>
       <button className="likebtn" onClick={likeClickHandler}>
-        Like Me!
-      </button>
-      <span style={{ backgroundColor: bgcolor1, color: color1 }}>
-        you got {likeCounter} likes
-      </span>
+        ॐ नमः शिवाय
+      </button>{" "}
+      {likeCounter}
+      <div className="bottom">
+        <small>
+          Made with <span>💛</span> by nksoftpune
+        </small>
+      </div>
     </div>
   );
 }
